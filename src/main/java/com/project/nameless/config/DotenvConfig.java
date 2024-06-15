fd494e0a8bfe0ce919953cc18d826e4a66758fda
edu9988 <edu_9988@hotmail.com>
2024-06-14 23:07:50 -0300

    Read environment variable using DotenvConfig

--- src/main/java/com/project/nameless/config/DotenvConfig.java
+++ src/main/java/com/project/nameless/config/DotenvConfig.java
@@ -13,19 +13,27 @@ public class DotenvConfig {
 
     @PostConstruct
     public void loadEnv() {
-        try {
-            List<String> lines = Files.readAllLines(Paths.get(".env"));
-            for (String line : lines) {
-                if (!line.trim().isEmpty() && !line.trim().startsWith("#")) {
-                    String[] parts = line.split("=", 2);
-                    if (parts.length == 2) {
-                        System.setProperty(parts[0], parts[1]);
-                    }
-                }
-            }
-        }
-	catch (IOException e) {
-	    throw new RuntimeException("Could not read .env file", e);
-        }
+	String dbPw = System.getenv( "DB_PW" );
+	if( dbPw != null ){
+	    System.out.println( "\nVariable DB_PW not null, using it\n" );
+	    System.setProperty( "DB_PW" , dbPw );
+	}
+	else{
+	    System.out.println( "\nVariable DB_PW null, trying .env\n" );
+	    try {
+		List<String> lines = Files.readAllLines(Paths.get(".env"));
+		for (String line : lines) {
+		    if (!line.trim().isEmpty() && !line.trim().startsWith("#")) {
+			String[] parts = line.split("=", 2);
+			if (parts.length == 2) {
+			    System.setProperty(parts[0], parts[1]);
+			}
+		    }
+		}
+	    }
+	    catch (IOException e) {
+		throw new RuntimeException("Could not read .env file", e);
+	    }
+	}
     }
 }
